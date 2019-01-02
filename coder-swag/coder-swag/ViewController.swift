2dc8d79d21adf522be85cf0fcacff899605810be
Thomas Donnelly <donnelly.thomas21@gmail.com>
2019-01-01 22:24:46 -0500

    initial commit

---
+++ coder-swag/coder-swag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coder-swag
+//
+//  Created by Thomas Donnelly on 1/1/19.
+//  Copyright © 2019 Thomas Donnelly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
