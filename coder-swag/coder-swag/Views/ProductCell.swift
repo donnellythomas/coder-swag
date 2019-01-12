794c81e9695cfda98c006538a98507ff8d2f6e68
Thomas Donnelly <donnelly.thomas21@gmail.com>
2019-01-12 10:47:12 -0500

    UICollectionView

---
+++ coder-swag/coder-swag/Views/ProductCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Thomas Donnelly on 1/12/19.
+//  Copyright © 2019 Thomas Donnelly. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
